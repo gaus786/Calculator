cdf5a62435d667dff311aa33681bc95af44238d3
gaus786 <124907883+gaus786@users.noreply.github.com>
2024-01-10 22:36:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let displayval='';
+document.querySelector('#inputBox').value=displayval;
+
+function enterval(data){
+    displayval+=data;
+    document.querySelector('#inputBox').value=displayval;
+
+}
